e69f41d8bfb3a6404964454d17d60c4f9b2696cb
filiptodosovski <filiptodosovski@gmail.com>
2024-11-10 11:33:34 +0100

    feat: added modes plugin for different color when selecting, neotree for file explorer and treesitter

---
+++ nvim/lua/plugins/neotree.lua
@@ -0,0 +1,14 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    cmd = "Neotree",
+    lazy = true,
+    keys = {
+      { "<C-b>", "<cmd>Neotree reveal right<cr>" },
+    },
+}
